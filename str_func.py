eeec02c6f8ffe46740fbf8f4448473ce2b61cc60
DaryaAgafonova <139240411+DaryaAgafonova@users.noreply.github.com>
2023-07-16 16:15:42 +0300

    Update str_func.py
    добавлен докстринг

--- str_func.py
+++ str_func.py
@@ -1,5 +1,8 @@
 def string_in_capital_letters(string):
 
+    """ Функция, которая принимает на вход строку
+    и возвращает ее со всеми заглавными буквами."""
+
     return string.upper()
 
 string = input()
